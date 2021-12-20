7924fdd9be0270a6c1cf93b3ea77029681a3af3c
Rahul Bramandlapalli <54270708+rbramand-xilinx@users.noreply.github.com>
2021-12-21 00:13:12 +0530

    Add changes to support RHEL 8.5 (#6073)
    * Add changes to support RHEL 8.5
    
    > Added changes to enable RHEL 8.5 support
    > fixed driver compilation issues
    > verified using xbutil validate on U2 card
    
    * Fix comments on PR
    
    Co-authored-by: root <root@localhost.localdomain>
    (cherry picked from commit 37518a9daf52faa89fef63cd116f92399df72cf1)

--- src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_bo.c
+++ src/runtime_src/core/pcie/driver/linux/xocl/userpf/xocl_bo.c
@@ -357,7 +357,7 @@ static struct drm_xocl_bo *xocl_create_bo(struct drm_device *dev,
 		xobj->metadata.state = DRM_XOCL_EXECBUF_STATE_ABORT;
 
 	obj = &xobj->base;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0) || defined(RHEL_8_5_GE)
 	obj->funcs = &xocl_gem_object_funcs;
 #endif
 
@@ -1173,7 +1173,7 @@ struct drm_gem_object *xocl_gem_prime_import_sg_table(struct drm_device *dev,
 		goto out_free;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0) || defined(RHEL_8_5_GE)
 	ret = drm_prime_sg_to_page_array(sgt, importing_xobj->pages,
 			attach->dmabuf->size >> PAGE_SHIFT);
 #else
@@ -1203,7 +1203,7 @@ out_free:
 	return ERR_PTR(ret);
 }
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0) && !defined(RHEL_8_5_GE)
 void *xocl_gem_prime_vmap(struct drm_gem_object *obj)
 {
 	struct drm_xocl_bo *xobj = to_xocl_bo(obj);
@@ -1260,7 +1260,7 @@ int xocl_gem_prime_mmap(struct drm_gem_object *obj, struct vm_area_struct *vma)
 		vma->vm_ops = xobj->dmabuf_vm_ops;
 	} else if (!IS_ERR_OR_NULL(xobj->base.dma_buf) && !IS_ERR_OR_NULL(xobj->base.dma_buf->file)) {
 		vma->vm_file = get_file(xobj->base.dma_buf->file);
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0) || defined(RHEL_8_5_GE)
 		vma->vm_ops = xobj->base.funcs->vm_ops;
 #else
 		vma->vm_ops = xobj->base.dev->driver->gem_vm_ops;
